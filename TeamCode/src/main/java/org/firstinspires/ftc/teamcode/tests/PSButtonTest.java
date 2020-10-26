c026468ecf11bf605a56169b0cfff12a8fe17ba6
Allen Naliath <allennaliath@gmail.com>
2020-10-26 16:33:15 -0400

    Created PSButtonTest to see if buttons pressed correspond with outputs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PSButtonTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.GamepadEventPS;
+
+@TeleOp
+public class PSButtonTest extends OpMode {
+    GamepadEventPS update;
+    boolean squarePressed = false;
+    boolean circlePressed = false;
+    boolean trianglePressed = false;
+    boolean xPressed = false;
+
+    @Override
+    public void init() {
+        update = new GamepadEventPS(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        squarePressed = false;
+        circlePressed = false;
+        trianglePressed = false;
+        xPressed = false;
+
+        if(gamepad1.square) squarePressed = true;
+        if(gamepad1.circle) circlePressed = true;
+        if(gamepad1.triangle) trianglePressed = true;
+        if(gamepad1.x) xPressed = true;
+
+        telemetry.addData("Square", squarePressed);
+        telemetry.addData("Circle", circlePressed);
+        telemetry.addData("Triangle", trianglePressed);
+        telemetry.addData("X", xPressed);
+        telemetry.update();
+    }
+}
